8a2d35763f77edd338d713a98849a07b8ae45824
zombeezmd <william.cox@208games.com>
2016-04-16 11:01:08 -0600

    Animation Phase 1

---
+++ Assets/Assets/Standard Assets/2D/Scripts/BearAttack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class BearAttack : MonoBehaviour {
+
+
+    private void Awake()
+    {
+        // Setting up references.
+        m_GroundCheck = transform.Find("GroundCheck");
+        m_CeilingCheck = transform.Find("CeilingCheck");
+        m_Anim = GetComponent<Animator>();
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
